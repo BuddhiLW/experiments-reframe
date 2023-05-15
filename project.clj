6cd1408932e68f4bf34d85564c7f4ff67b8b0b5c
Pedro Branquinho <pedrogbranquinho@gmail.com>
2023-05-15 20:09:57 -0300

    UP: start backend for heroku deploy

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject cheffy "0.1.0-SNAPSHOT"
+  :description "Playground backend"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.10.0"]
+                 [integrant "0.8.0"]
+                 [environ "1.2.0"]
+                 [metosin/reitit "0.7.0-alpha3"]
+                 [com.github.seancorfield/next.jdbc "1.3.874"]
+                 [org.postgresql/postgresql "42.6.0"]
+                 [clj-http "3.12.3"]
+                 [net.clojars.kelveden/ring-jwt "2.4.0"]]
+  :profiles {:uberjar {:aot :all}
+             :dev {:source-paths ["dev/src"]
+                   :resource-paths ["dev/resources"]
+                   :dependencies [[ring/ring-mock "0.4.0"]
+                                  [integrant/repl "0.3.2"]]}}
+  :uberjar-name "playground.jar")
+
+;; :license {:name "Eclipse Public License"
+;;           :url "http://www.eclipse.org/legal/epl-v10.html"}
+;; :main ^:skip-aot playground.server
+;; :target-path "target/%s"
---
+++ test/playground/core_test.clj
@@ -0,0 +1,2 @@
+(ns playground.core-test)
+
