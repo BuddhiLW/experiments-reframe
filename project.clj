0fed133a5188a23ece3c446045b2910435faa0ac
Pedro Branquinho <pedrogbranquinho@gmail.com>
2023-05-15 20:18:13 -0300

    min-lein-version 2.0.0

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject cheffy "0.1.0-SNAPSHOT"
   :description "Playground backend"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring/ring-core "1.10.0"]
                  [integrant "0.8.0"]
